839d5f99241ace85ef07549821370b3000d63554
Kariloko22 <102458440+Kariloko22@users.noreply.github.com>
2022-03-28 20:30:31 -0300

    main

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
   });
+    document.querySelector(".nav-menu").addEventListener("click", () => {
+      document.querySelector(".nav-menu").classList.toggle("show");
+    
+  });
     // 360 viewer
     var PSV = new PhotoSphereViewer({
       panorama: "https://thumbs.dreamstime.com/z/fotos-esf%C3%A9ricas-equirectangulares-flamingo-park-miami-beach-foto-equirectangular-esf%C3%A9rica-estados-unidos-166112464.jpg",
